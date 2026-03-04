5239e7b1f87ac6fe08780aa8221d192376c1d538
smoothie-ws <kibot.email@gmail.com>
2026-03-04 23:55:13 +1000

    test ui markup dsl

--- src/mutts/ui/playground/Playground.hx
+++ src/mutts/ui/playground/Playground.hx
@@ -1,12 +1,19 @@
 package mutts.ui.playground;
 
-import s2d.Element;
-
-class Playground extends Element {
-	@ui.markup
+@:ui.short(playground)
+class Playground extends s2d.Element {
+	@:ui.markup
 	function build() {
-		@Element(width = 2) {
-			@Element(width = parent.width) {}
+		@element(width = 2, {
+			height: 10,
+			onHeightChanged: v -> trace(parent.height)
+		}) {}
+		@ground(width = parent.width) {
+			for (i in 0...10)
+				@image(height = 2) {}
 		}
 	}
 }
+
+@:ui.shortcut(ground)
+class Ground extends s2d.Element {}
