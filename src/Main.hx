9ddaf3c32706917310dcf8659a81505fc51aa357
smoothie <kibot.email@gmail.com>
2026-03-15 20:49:11 +1000

    update main

--- src/Main.hx
+++ src/Main.hx
@@ -1,31 +1,27 @@
 package;
 
-import s2d.elements.Interactive;
-import s2d.elements.shapes.RectangleRounded;
-import se.animation.Easing;
 import se.App;
-import se.system.input.Mouse;
+import se.Timer;
+import se.math.SMath;
+import se.animation.Easing;
 import se.animation.ColorAnimation;
 import s2d.Element;
 import s2d.WindowScene;
 import s2d.elements.Text;
+import s2d.elements.InteractiveElement;
+import s2d.elements.shapes.RectangleRounded;
 
 @:app.title("Mutts")
 @:app.window(width = 750, height = 500)
 class Main extends se.App implements s2d.Markup {
 	public static function main() {
 		var scene = new WindowScene(window);
-		scene.color = Blue;
 		scene.root.padding = 50;
 		markup(scene.root);
 	}
 
 	@:ui.style
 	static function style() {
-		@drawable {
-			$color = Red;
-		}
-
 		@text {
 			$alignment = AlignCenter;
 			$fontSize = 32;
@@ -36,20 +32,30 @@ class Main extends se.App implements s2d.Markup {
 	static function markup() {
 		@use style;
 
-		var rect = @interactive(rectangle.rounded) {
-			$onMouseScrolled(m -> rect.radius += m.delta * 10);
-			$onMouseEntered((x, y) -> new ColorAnimation($color, White, 0.5, c -> $color = c).ease(Easing.OutQuart).start());
-			$onMouseExited((x, y) -> new ColorAnimation($color, Black, 0.5, c -> $color = c).ease(Easing.OutQuart).start());
-			$onHoveredDirty(h -> se.App.input.mouse.cursor = h ? Pointer : Default);
-
+		var rect = @rectangle {
 			$anchors.fill($parent);
 			$anchors.margins = 50;
 
-			@text("Hello, world!") {
-				$tag = "text";
+			new Timer(() -> $rotate(radians(1), vec2(375, 250)), 0.01).loop();
 
+			@text("Hello, world!") {
+				$anchors.fill($parent);
+				$tag = "text";
 				$color = Black;
-				$anchors.fill(rect);
+			}
+
+			@interactive {
+				$anchors.fill($parent);
+
+				$onMouseEntered((x, y) -> new ColorAnimation(rect.color, Green, 0.5, c -> rect.color = c).ease(Easing.OutQuart).start());
+				$onMouseExited((x, y) -> new ColorAnimation(rect.color, Red, 0.5, c -> rect.color = c).ease(Easing.OutQuart).start());
+				$onHoveredDirty(h -> {
+					se.App.input.mouse.cursor = h ? Pointer : Default;
+					if (h)
+						rect.upscale(1.2, vec2(375, 250));
+					else
+						rect.upscale(1 / 1.2, vec2(375, 250));
+				});
 			}
 		}
 	}
