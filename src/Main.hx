3de9e7fdc6512f4010b2486d87bacc75f311be05
smoothie-ws <kibot.email@gmail.com>
2026-03-23 12:51:09 +1000

    test custom text renderer

--- src/Main.hx
+++ src/Main.hx
@@ -36,104 +36,102 @@ class Main extends s.App implements s.markup.Markup {
 			$tag = "rectangle";
 			$padding = 50;
 
-			@rectangle {
+			// @rectangle {
+			// 	$anchors.fill($parent);
+			// 	$color = Red;
+
+			// 	var r = @rectangle {
+			// 		$margins = 50;
+			// 		$width = 100;
+			// 		$anchors.top = $parent.top;
+			// 		$anchors.left = $parent.left;
+			// 		$anchors.bottom = $parent.bottom;
+			// 		$color = Green;
+
+			// 		@ellipse(30) {
+			// 			$anchors.fill($parent);
+			// 			$color = Black;
+			// 		}
+			// 	}
+
+			// var img = @image("teoria_veroyatnostei2") {
+			// 	$fillMode = Contain;
+			// 	$anchors.fill($parent);
+			// 	// $layout.fillWidth = true;
+			// 	// $layout.fillWidthFactor = 0.5;
+			// 	// $layout.fillHeight = true;
+			// 	// $layout.fillHeightFactor = 0.5;
+			// 	// $layout.alignment = AlignVCenter | AlignLeft;
+
+			// 	App.input.mouse.onButtonPressed(Left, (x, y) -> {
+			// 		img.sampling = switch img.sampling {
+			// 			case Nearest: Bilinear;
+			// 			case Bilinear: Prefiltered;
+			// 			case Prefiltered: Trilinear;
+			// 			case Trilinear: Nearest;
+			// 		}
+			// 	});
+			// }
+
+			// @rectangle {
+			// 	$margins = 50;
+			// 	$padding = 50;
+			// 	$anchors.top = $parent.top;
+			// 	$anchors.bottom = $parent.bottom;
+			// 	$anchors.left = r.right;
+			// 	$anchors.right = $parent.right;
+			// 	$color = Black;
+
+			// 	var grad = @gradient.linear {
+			// 		$stops = [
+			// 			{color: White, position: 0.0},
+			// 			{color: Red, position: 0.5},
+			// 			{color: Black, position: 1.0}
+			// 		];
+			// 		$interpolation = Interpolation.InQuart;
+			// 		$anchors.fill($parent);
+			// 		$padding = 50;
+			// 		$width = 100;
+
+			// 		App.input.mouse.onMoved((x, y, dx, dy) -> $start = grad.mapFromGlobalNormalized(x, y));
+
+			// 		@triangle(10) {
+			// 			$border.width = 5;
+			// 			$border.color = Blue;
+			// 			$color = Yellow;
+			// 			$anchors.fill($parent);
+			// 		}
+
+			// 		@box {
+			// 			$anchors.fill($parent);
+
+			// 			App.input.mouse.onScrolled(d -> $padding = $left.padding + d * 10);
+
+			@label("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") {
 				$anchors.fill($parent);
-				$color = Red;
+				$color = Green;
+				$fontSize = 32;
+				$elideMode = ElideLeft;
+				// $wrapMode = WrapAnywhere;
+				$alignment = AlignBottom;
 
-				var r = @rectangle {
-					$margins = 50;
-					$width = 100;
-					$anchors.top = $parent.top;
-					$anchors.left = $parent.left;
-					$anchors.bottom = $parent.bottom;
-					$color = Green;
-
-					@ellipse(30) {
-						$anchors.fill($parent);
-						$color = Black;
-					}
-				}
-
-				@rectangle {
-					$margins = 50;
-					$padding = 50;
-					$anchors.top = $parent.top;
-					$anchors.bottom = $parent.bottom;
-					$anchors.left = r.right;
-					$anchors.right = $parent.right;
-					$color = Black;
-
-					var grad = @gradient.linear {
-						$stops = [
-							{color: White, position: 0.0},
-							{color: Red, position: 0.5},
-							{color: Black, position: 1.0}
-						];
-						$interpolation = Interpolation.InQuart;
-						$anchors.fill($parent);
-						$padding = 50;
-						$width = 100;
-
-						App.input.mouse.onMoved((x, y, dx, dy) -> $start = grad.mapFromGlobalNormalized(x, y));
-
-						@triangle(10) {
-							$border.width = 5;
-							$border.color = Blue;
-							$color = Yellow;
-							$anchors.fill($parent);
-						}
-
-						@box {
-							$anchors.fill($parent);
-
-							App.input.mouse.onScrolled(d -> $padding = $left.padding + d * 10);
-
-							var img = @image("cert") {
-								$fillMode = Contain;
-								$layout.fillWidth = true;
-								$layout.fillWidthFactor = 0.5;
-								$layout.fillHeight = true;
-								$layout.fillHeightFactor = 0.5;
-								$layout.alignment = AlignVCenter | AlignLeft;
-							}
-
-							@text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") {
-								$layout.fillWidth = true;
-								$layout.fillWidthFactor = 0.5;
-								$layout.fillHeight = true;
-								$layout.fillHeightFactor = 0.5;
-								$layout.alignment = AlignRight;
-								$color = Green;
-								$fontSize = 32;
-								$elideMode = ElideLeft;
-								$wrapMode = WrapAnywhere;
-								$alignment = AlignBottom;
-
-								App.input.mouse.onButtonPressed(Left, (x, y) -> {
-									img.quality = switch img.quality {
-										case Low:
-                                            Middle;
-										case Middle:
-                                            Good;
-										case Good:
-                                            Best;
-										case Best:
-                                            Low;
-									}
-
-									if ($alignment & AlignLeft != 0)
-										$alignment = AlignHCenter;
-									else if ($alignment & AlignHCenter != 0)
-										$alignment = AlignRight;
-									else
-										$alignment = AlignLeft;
-									$alignment = $alignment | AlignBottom;
-								});
-							}
-						}
-					}
-				}
+				new Timer(() -> $transform.rotation += 0.01, 0.01).loop();
+				
+				App.input.mouse.onButtonPressed(Left, (x, y) -> {
+					if ($alignment & AlignLeft != 0)
+						$alignment = AlignHCenter;
+					else if ($alignment & AlignHCenter != 0)
+						$alignment = AlignRight;
+					else
+						$alignment = AlignLeft;
+					$alignment = $alignment | AlignBottom;
+				});
 			}
+
+			// 		}
+			// 	}
+			// }
+			// }
 		}
 	}
 }
